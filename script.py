00c4fc9130c82d54949deb20bdfaf3a1643ca79d
adhanani90 <77396180+adhanani90@users.noreply.github.com>
2023-01-11 18:42:12 -0700

    Second commit

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ import requests
 print(requests.__version__)
 
 print(requests.get('https://www.google.com/'))
+
+r = requests.get("https://raw.githubusercontent.com/adhanani90/cmput_404_lab1/master/script.py")
+print(r.text)
